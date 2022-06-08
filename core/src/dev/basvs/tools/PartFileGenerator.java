4c55d4d24e3757336218298b02ad95634a7b37a6
Bas <b.vanschoonhoven@gmail.com>
2022-06-08 11:17:21 +0200

    PartFileGenerator license and dependency fix

--- core/src/dev/basvs/tools/PartFileGenerator.java
+++ core/src/dev/basvs/tools/PartFileGenerator.java
@@ -6,31 +6,13 @@ import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.badlogic.gdx.utils.Json;
 import dev.basvs.crashlander.lander.part.PartDesign;
 import dev.basvs.crashlander.lander.part.Tank;
 import dev.basvs.crashlander.lander.part.Thruster;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.Json;
-
-/**
- * Copyright (c) 2013, Bas van Schoonhoven. All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
- * following conditions are met: Redistributions of source code must retain the above copyright notice, this list of
- * conditions and the following disclaimer. Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation and/or other materials provided with the
- * distribution.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 
 public class PartFileGenerator {
 
